ebd31534f43af6860a4c7cc34063b7b91907a8c6
AlenaD-B27 <lenador.18@gmail.com>
2022-09-22 10:56:23 -0500

    Login text error message verification

---
+++ src/test/java/com/cydeo/tests/da3_locators_sccSelector_xpath/Locators_getText.java
@@ -0,0 +1,65 @@
+package com.cydeo.tests.da3_locators_sccSelector_xpath;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Locators_getText {
+    /*
+    1- Open a chrome browser
+2- Go to: https://login1.nextbasecrm.com/
+3- Enter incorrect username: “incorrect”
+4- Enter incorrect password: “incorrect”
+5- Click to login button.
+6- Verify error message text is as expected:
+Expected: Incorrect login or password
+     */
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get("https://login1.nextbasecrm.com/");
+
+       // driver.findElement(By.name("USER_LOGIN")).sendKeys("incorrect");
+        WebElement inputUserName = driver.findElement(By.name("USER_LOGIN"));
+        inputUserName.sendKeys("incorrect");
+
+        WebElement inputPassword = driver.findElement(By.name("USER_PASSWORD"));
+        inputPassword.sendKeys("incorrect");
+
+        WebElement loginBtn = driver.findElement(By.className("login-btn"));
+        loginBtn.click();
+
+        //================================================================
+
+        WebElement errorMessage = driver.findElement(By.className("errortext"));
+        String expectedErrorMessage = "Incorrect login or password";
+        String actualErrorMessage = errorMessage.getText();
+
+        if(actualErrorMessage.equals(expectedErrorMessage)){
+            System.out.println("Error message verification test PASSED");
+        }else{
+            System.out.println("Error message verification test FAILED");
+        }
+
+
+        driver.quit();
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
