726b5330ad4e04eaad5c213539c75e5c20c5a695
AlenaD-B27 <lenador.18@gmail.com>
2022-09-29 12:05:29 -0500

    StaleElementReferenceException ex

---
+++ src/test/java/com/cydeo/tests/day_5/StaleElementReference_Exception.java
@@ -0,0 +1,48 @@
+package com.cydeo.tests.day_5;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.concurrent.TimeUnit;
+
+public class StaleElementReference_Exception {
+
+    public static void main(String[] args) {
+
+        /*
+        1. Open Chrome browser
+2. Go to https://practice.cydeo.com/add_remove_elements/
+3. Click to “Add Element” button
+4. Verify “Delete” button is displayed after clicking.
+5. Click to “Delete” button.
+6. Verify “Delete” button is NOT displayed after clicking. USE
+
+         */
+        WebDriver driver = WebDriverFactory.getDriver("Chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        driver.get("https://practice.cydeo.com/add_remove_elements/");
+
+        WebElement addElementBtn = driver.findElement(By.xpath("//button[.='Add Element']"));
+        addElementBtn.click();
+
+        WebElement deleteButton = driver.findElement(By.xpath("//button[.='Delete']"));
+        deleteButton.click();
+
+        try{
+            System.out.println("deleteButton.isDisplayed() = " + deleteButton.isDisplayed());
+        } catch (org.openqa.selenium.StaleElementReferenceException e){
+            System.out.println("Stale element reference exception i thrown and this is already expected");
+        }
+
+        driver.quit();
+
+
+    }
+
+
+
+}
