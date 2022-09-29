613c293153de49adf01d048c91e6394a7cdcf86c
AlenaD-B27 <lenador.18@gmail.com>
2022-09-29 15:49:11 -0500

    TestNG intro

---
+++ src/test/java/com/cydeo/tests/day_5_testNG_dropdown/TestNG_Intro.java
@@ -0,0 +1,51 @@
+package com.cydeo.tests.day_5_testNG_dropdown;
+
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+public class TestNG_Intro {
+
+    @Test (priority = 1)
+    public void test1(){
+        System.out.println("test 1 is running...");
+        String actual = "apple";
+        String expected = "apple";
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test (priority = 2)
+    public void test2(){
+        System.out.println("test 2 is running...");
+        String actual = "apple";
+        String expected = "apple2";
+
+        Assert.assertTrue(actual.equals(expected));
+
+    }
+
+    @BeforeMethod
+    public void setUpMethod(){
+        System.out.println(" ---> Before Method is running");
+    }
+
+    @AfterMethod
+    public void tearDownMethod(){
+        System.out.println(" ---> After Method is running");
+    }
+
+    @BeforeClass
+    public void setUp(){
+        System.out.println("--> Before Class is running");
+    }
+
+    @AfterClass
+    public void tearDown(){
+        System.out.println("--> After class is running");
+    }
+
+
+
+
+
+}
---
+++ src/test/java/com/cydeo/tests/day_5_testNG_dropdown/TestNG_Selenium.java
@@ -0,0 +1,48 @@
+package com.cydeo.tests.day_5_testNG_dropdown;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestNG_Selenium {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUpMethod(){
+        // 1. Set up browser
+        driver = WebDriverFactory.getDriver("Chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        // 2. Navigate to the Google page
+        driver.get("https://www.google.com");
+    }
+
+
+
+
+    @Test
+    public void googleTitle(){
+
+
+
+        // 3. Verify if title is "Google"
+        String actualTitle = driver.getTitle();
+        String expectedTitle = "Google";
+
+        Assert.assertEquals(actualTitle, expectedTitle, "Title is not matching");
+
+    }
+
+    @AfterMethod
+    public void tearDownMethod(){
+        // Kill the session
+        driver.quit();
+    }
+}
