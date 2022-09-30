3e263801734d6e85cf2e006cb52384f090d9e701
AlenaD-B27 <lenador.18@gmail.com>
2022-09-29 21:09:53 -0500

    Simple dropdown

---
+++ src/test/java/com/cydeo/tests/day_5_testNG_dropdown/SimpleDropdown.java
@@ -0,0 +1,69 @@
+package com.cydeo.tests.day_5_testNG_dropdown;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+
+public class SimpleDropdown {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setupMethod(){
+        driver= WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void simpleDropdown(){
+        /*
+2. Go to https://practice.cydeo.com/dropdown
+3. Verify “Simple dropdown” default selected value is correct
+Expected: “Please select an option”
+4. Verify “State selection” default selected value is correct
+Expected: “Select a State”
+         */
+        driver.get("https://practice.cydeo.com/dropdown");
+
+        Select simpleDropdown = new Select(driver.findElement(By.xpath("//select[@id='dropdown']")));
+
+
+
+        String actualSimpleDropdown = simpleDropdown.getFirstSelectedOption().getText();
+        String expectedSimpleDropdown = "Please select an option";
+
+        Assert.assertEquals(actualSimpleDropdown, expectedSimpleDropdown);
+
+        Select stateDropdown = new Select(driver.findElement(By.xpath("//select[@id='state']")));
+        String actualStateDropdown = stateDropdown.getFirstSelectedOption().getText();
+        String expectedStateDropdown = "Select a State";
+
+        Assert.assertEquals(actualStateDropdown, actualStateDropdown);
+
+    }
+
+
+    @AfterMethod
+    public void tearDownMethod(){
+        driver.quit();
+    }
+
+
+
+
+
+
+
+
+
+
+}
