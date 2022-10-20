a88e231fb193b06723ae71cb3520b97815bcb9f8
AlenaD-B27 <lenador.18@gmail.com>
2022-10-20 13:49:39 -0500

    POM practice

--- src/test/java/com/cydeo/tests/day13_pom_explicitwait/LibraryLoginTest.java
+++ src/test/java/com/cydeo/tests/day13_pom_explicitwait/LibraryLoginTest.java
@@ -4,17 +4,19 @@ import com.cydeo.pages.LibraryLoginPage;
 import com.cydeo.utilities.Driver;
 import com.github.javafaker.Faker;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LibraryLoginTest {
 
-    LibraryLoginPage libraryLoginPage = new LibraryLoginPage();
+    LibraryLoginPage libraryLoginPage;
     Faker faker = new Faker();
 
     @BeforeMethod
     public void setUp(){
         Driver.getDriver().get("https://library1.cydeo.com/");
+        libraryLoginPage = new LibraryLoginPage();
     }
 
 
@@ -59,6 +61,29 @@ public class LibraryLoginTest {
         Assert.assertTrue(libraryLoginPage.enterInvalidEmailErrorMsg.isDisplayed());
 
 
+    }
+
+    @Test
+    public void library_negative_login_test(){
+        //1- Open a chrome browser
+        //2- Go to: https://library1.cydeo.com
+        //3- Enter incorrect username or incorrect password
+
+        libraryLoginPage.inputUsername.sendKeys("wrong@email.com");
+        libraryLoginPage.inputPassword.sendKeys("wrongpassword");
+        libraryLoginPage.signInBtn.click();
+
+        //4- Verify title expected error is displayed:
+        //Expected: Sorry, Wrong Email or Password
+        Assert.assertTrue(libraryLoginPage.wrongEmailAndPasswordErrorMsg.isDisplayed());
+
+    }
+
+
+
+    @AfterMethod
+    public void tearDown(){
+        Driver.closeDriver();
     }
 
 
